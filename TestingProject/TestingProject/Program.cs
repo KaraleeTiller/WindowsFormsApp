ff09bf90dcc4095b4e65763e38ee238c8a18bcb6
programmer <programmer@BB21008>
2019-04-16 18:12:26 -0500

    This is a WriteLine and ReadKey method

---
+++ TestingProject/TestingProject/Program.cs
@@ -0,0 +1,17 @@
+﻿// Karalee
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
